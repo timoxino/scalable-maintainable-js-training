7b3b3a5afb8230f62f4cd9f4b26f3d071fcb4818
Tsimafei Shchytkavets <Tsimafei_Shchytkavets@epam.com>
2013-11-10 13:02:51 +0300

    Construct ResultLiteral via init function

--- quiz_part_2/application.js
+++ quiz_part_2/application.js
@@ -1,8 +1,14 @@
 var APP = (function($, APP) {
+
 	APP.init = function() {
 		$.getJSON("questions.json", function(data) {
 			new APP.QuizClass(data);
 		});
+		$.getJSON("results.json", function(data) {
+			APP.ResultLiteral.init(data);
+		});
 	}
+
 	return APP;
+
 })(jQuery, APP || {});
--- quiz_part_2/result.js
+++ quiz_part_2/result.js
@@ -2,25 +2,21 @@ var APP = APP || {};
 
 APP.ResultLiteral = (function(APP, $, undefined) {
 
-    return {
-
-        renderResult: function(points) {
-            $.getJSON("results.json", function(data) {
-                renderAnswer(points, data)
-            });
-
-            function renderAnswer(points, data) {
-                $.each(data, function(index, value) {
-                    if (points <= value.to) {
-                        $("#resultContainer").append(data[index].status + "(" + points + ")");
-                        return false;
-                    }
-                });
+    function renderAnswer(points, data) {
+        $.each(data, function(index, value) {
+            if (points <= value.to) {
+                $("#resultContainer").append(data[index].status + "(" + points + ")");
+                return false;
             }
+        });
+    }
+
+    return {
+        init: function(data) {
+            APP.EventBus.bind("showResults", function(points) {
+                renderAnswer(points, data);
+            });
         }
     };
-})(APP, jQuery);
 
-APP.EventBus.bind("showResults", function(points) {
-    APP.ResultLiteral.renderResult(points);
-});
+})(APP, jQuery);
